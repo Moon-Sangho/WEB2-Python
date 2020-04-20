fbce83ffcd4e95fe904f41d5e7164d9ca64385a3
Moon-Sangho <62928788+Moon-Sangho@users.noreply.github.com>
2020-04-20 18:57:39 +0900

    Changing the description by separating data files

--- index.py
+++ index.py
@@ -5,8 +5,10 @@ import cgi #'cgi라는 모듈을 사용하겠다''라고 파이썬에게 얘기�
 form = cgi.FieldStorage()
 if 'id' in form:
     pageId = form["id"].value
+    description = open('data/'+pageId, 'r').read()
 else:
     pageId = 'Welcome'
+    description = 'Hello web'
 print('''<!DOCTYPE html>
 <html>
   <head>
@@ -24,10 +26,8 @@ print('''<!DOCTYPE html>
       </ol>
       <div id="article">
     <h2>{title}</h2>
-    <p>
-      The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
-    </p>
+    <p>{desc}</p>
       </div>
     </div>
   </body>
-</html>'''.format(title=pageId))
+</html>'''.format(title=pageId, desc=description))
