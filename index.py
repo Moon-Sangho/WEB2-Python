b68633a87746e55c186bf9ae1ea8d4f4a2f1118a
Moon-Sangho <62928788+Moon-Sangho@users.noreply.github.com>
2020-04-23 15:13:21 +0900

    Add Sanitize function
    웹사이트에 Sanitize 기능을 추가하여 사용자가 불순한 기능(?)을 게시했을 때, 그것을 걸러주는 효과를 얻을 수 있다.

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
 #!python
 print("Content-Type: text/html; charset=utf-8\n")
 print()
-import cgi, os, view
+import cgi, os, view, html_sanitizer
+sanitizer = html_sanitizer.Sanitizer()
 
 form = cgi.FieldStorage()
 if 'id' in form:
-    pageId = form["id"].value
+    title = pageId = form["id"].value
     description = open('data/'+pageId, 'r').read()
+    title = sanitizer.sanitize(title)
+    description = sanitizer.sanitize(description)
     update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
     delete_action = '''
         <form action="process_delete.py" method="post">
@@ -15,7 +18,7 @@ if 'id' in form:
         </form>
     '''.format(pageId)
 else:
-    pageId = 'Welcome'
+    title = pageId = 'Welcome'
     description = 'Hello web'
     update_link = ''
     delete_action = ''
@@ -43,7 +46,7 @@ print('''<!DOCTYPE html>
   </body>
 </html>
 '''.format(
-    title=pageId,
+    title=title,
     desc=description,
     listStr=view.getList(),
     update_link=update_link,
