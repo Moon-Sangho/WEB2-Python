77df560eb63a898dce8e1c10ce24e6bdd0799315
Moon-Sangho <62928788+Moon-Sangho@users.noreply.github.com>
2020-04-23 13:19:32 +0900

    Implementing the delete function
    삭제 기능을 구현함으로써 사용자가 웹 페이지의 내용을 삭제할 수 있도록 할 수 있다.

--- index.py
+++ index.py
@@ -13,10 +13,17 @@ if 'id' in form:
     pageId = form["id"].value
     description = open('data/'+pageId, 'r').read()
     update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
+    delete_action = '''
+        <form action="process_delete.py" method="post">
+            <input type="hidden" name="pageId" value="{}">
+            <input type="submit" value="delete">
+        </form>
+    '''.format(pageId)
 else:
     pageId = 'Welcome'
     description = 'Hello web'
     update_link = ''
+    delete_action = ''
 print('''<!DOCTYPE html>
 <html>
   <head>
@@ -37,5 +44,6 @@ print('''<!DOCTYPE html>
     </div>
     <a href="create.py">create</a>
     {update_link}
+    {delete_action}
   </body>
-</html>'''.format(title=pageId, desc=description, listStr=listStr, update_link=update_link))
+</html>'''.format(title=pageId, desc=description, listStr=listStr, update_link=update_link, delete_action=delete_action))
